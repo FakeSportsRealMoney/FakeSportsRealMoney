c3473888beda862b9da8f1543d5e18e8ec79d1fb
dylanjsa90 <dylanjsanders1@gmail.com>
2016-08-03 16:07:38 -0700

    created basic user and league models and league schema methods

---
+++ models/league.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const User = require('./models/user');
+
+let leagueSchema = mongoose.Schema({
+  name: String,
+  sport: String
+});
+
+leagueSchema.methods.addUser = function(userData) {
+  let user = new User(userData);
+  user.leagueId = this._id;
+  return user.save();
+};
+
+
+// Need to set what gets updated either here or on the return call
+leagueSchema.methods.updateUser = function(userId) {
+  return User.findOneAndUpdate({'_id':userId});
+};
+
+leagueSchema.methods.removeUser = function(userId) {
+  return User.findOneAndUpdate({'_id': userId}, {leagueId: null});
+};
+
+leagueSchema.methods.findAllLeagueMembers = function() {
+  return User.find({leagueId: this._id});
+};
+
+leagueSchema.methods.findOverdueMembers = function() {
+  return User.find({leagueId: this.id, overdue: true});
+};
+
+module.exports = exports = mongoose.model('league', leagueSchema);
--- models/user.js
+++ models/user.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const mongoose = require('mongoose');
+
+let userSchema = mongoose.Schema({
+  name: {type: String, required: true},
+  overdue: {type: Boolean, required: true},
+  amountDue: Number,
+  contact: {
+    phone: {type: String, required: true},
+    email: {type: String, required: true}
+  },
+  leagueId: String,
+  role: {type: String, default: 'basic'}
+});
+
+module.exports = exports = mongoose.model('user', userSchema);
