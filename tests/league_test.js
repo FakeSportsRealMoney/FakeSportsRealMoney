936af27db0290a1ce7018fc1d2bf3985277d5742
dylanjsa90 <dylanjsanders1@gmail.com>
2016-08-05 10:50:52 -0700

    minor test work

--- tests/league_test.js
+++ tests/league_test.js
@@ -3,7 +3,8 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
-const expect = chai.request;
+const expect = chai.expect;
+const request = chai.request;
 const baseUrl = 'localhost:5000/api';
 const League = require('../models/league');
 
@@ -11,7 +12,11 @@ describe('league CRUD tests', function() {
   it('should create a league', function(done) {
     request(baseUrl)
       .post('/')
-      .send()
-      done();
+      .send({name: 'fakeLeague'})
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res).to.be(res);
+        done();
+      });
   });
 });
