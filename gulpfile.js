49d87c17f2b9e9295eacf9fdbbc2d3cde3b6fb72
dylanjsa90 <dylanjsanders1@gmail.com>
2016-08-09 10:23:44 -0700

    minor gulpfile changes

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,18 @@ const mocha = require('gulp-mocha');
 const eslint = require('gulp-eslint');
 
 let appFiles = ['./server.js', './lib/*.js', './models/*.js', '/routes/*.js'];
-let testFiles = ['./tests/*.js'];
 
 gulp.task('lint:all', () => {
   gulp.src(appFiles)
     .pipe(eslint())
     .pipe(eslint.format());
-  gulp.src(testFiles)
+  gulp.src('./tests/test_harness.js')
     .pipe(eslint())
     .pipe(eslint.format());
 });
 
 gulp.task('mocha', () => {
-  gulp.src(testFiles)
+  gulp.src('./tests/test_harness.js')
     .pipe(mocha());
 });
 
