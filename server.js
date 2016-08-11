606832edf3d6844dfda6af1eb56e88c7c0b4a865
dylanjsa90 <dylanjsanders1@gmail.com>
2016-08-11 13:19:17 -0700

    removed sendgrid

--- server.js
+++ server.js
@@ -7,12 +7,10 @@ const Promise = require('./lib/promise');
 const leagueRouter = require('./routes/league_router');
 const userRouter = require('./routes/user_router');
 const cronJobs = require('./lib/cron');
-const sendgrid = require('sendgrid')(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
 const PORT = process.env.PORT || 3000;
 
 const dbPort = process.env.MONGODB_URI || 'mongodb://localhost/dev_db';
-const dotenv = require('dotenv')();
-dotenv.load();
+
 mongoose.Promise = Promise;
 mongoose.connect(dbPort);
 
@@ -29,36 +27,3 @@ cronJobs.overdueCheck;
 cronJobs.veryOverdue;
 
 app.listen(PORT, () => console.log('server up on port ' + PORT));
-
-var inbound = {
-  handler: function(req) {
-    let contents = req.payload;
-    let envelope;
-    let to;
-    if (contents.envelope) { envelope = JSON.parse(contents.envelope); }
-    if (envelope) { to = envelope.from; }
-
-    var Email = sendgrid.Email;
-    let email = new Email({
-      to: 'dylanjsanders1@gmail.com',
-      from: to,
-      subject: 'sendgrid test',
-      text: contents
-    });
-
-    sendgrid.send(email, (err, json) => {
-      if (err) {
-        console.log(err);
-        req.reply({success: false, error: {message:err.message}});
-      } else {
-        req.reply({success:true});
-      }
-    });
-  }
-};
-
-app.addRoute({
-  method: 'POST',
-  path: '/inbound',
-  config: inbound
-});
